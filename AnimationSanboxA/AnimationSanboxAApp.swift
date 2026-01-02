9d32e2a6cef24e63fdb153bf16918ec85c73dbd7
superkookai <weerawut@live.com>
2026-01-02 09:17:42 +0700

    Initial Commit

---
+++ AnimationSanboxA/AnimationSanboxAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSanboxAApp.swift
+//  AnimationSanboxA
+//
+//  Created by Weerawut on 2/1/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSanboxAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
